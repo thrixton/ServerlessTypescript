235fe43f54cd0396658fe053f8c551425a03db52
John Baro <johnbaro@gmail.com>
2018-10-01 20:43:44 +1000

    Broken by @aws/dynamodb-data-mapper

--- handler.ts
+++ handler.ts
@@ -1,13 +1,32 @@
-import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
+import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import { DataMapper } from "@aws/dynamodb-data-mapper";
+import { hashKey, table } from "@aws/dynamodb-data-mapper-annotations";
+import { DynamoDB } from "aws-sdk";
 
-export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+const hello: Handler = (
+  event: APIGatewayEvent,
+  context: Context,
+  cb: Callback
+) => {
+  const mapper = new DataMapper({
+    client: new DynamoDB(),
+    tableNamePrefix: `dev-`
+  });
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   cb(null, response);
+};
+
+@table("test")
+export class Test {
+  @hashKey()
+  public id: string;
 }
